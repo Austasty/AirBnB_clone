4d4f5fa3595cdc63afe8c73ddc78dd677b936f1e
root <root@DESKTOP-TF3DUK0>
2023-12-10 22:31:30 +0100

    FilStorage Modification

---
+++ console.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import cmd
+
+
+class HbnbCommand(cmd.Cmd):
+    """HBNB Console class definition"""
+    
+    prompt = "(Enter Command)"
+    
+    
+    
+
+
+
+if __name__ == "__main__":
+    HbnbCommand().cmdloop()
