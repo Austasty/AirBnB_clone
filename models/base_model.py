9eca335e630844f708eb7d71e1636bf9dd460661
emmieIO <markonuoha6@gmail.com>
2023-12-11 22:54:33 +0100

    Working on Test and errors

--- models/base_model.py
+++ models/base_model.py
@@ -26,8 +26,8 @@ class BaseModel:
 
     def __str__(self) -> str:
         """Prints string representaion of a model"""
-        return "[{}]({}) {}".format(self.__class__.__name__,
-                                    self.id, self.__dict__)
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """updates the public instance attribute
